145e5cced078fad2cedcc08755c9e048622295f3
Caden <cadenjlund@gmail.com>
2025-01-11 06:05:02 -0600

    updated light and dark mode

--- CadenSite/src/components/Header.tsx
+++ CadenSite/src/components/Header.tsx
@@ -5,13 +5,20 @@ import AnimatedToggle from "./AnimatedToggle"; // Import the toggle component
 
 const Header: React.FC = () => {
     return (
-        <header className=" text-black dark:text-white">
+        <header className=" text-black dark:text-white sticky top-0">
             <div className="px-[2rem] py-4 mt-3 flex justify-between items-center w-full ">
                 {/* Left side: Logo and Navigation Links */}
                     <Link to="/" className="text-2xl font-bold">
-                        CadenLund
+                        <div className="flex-col">
+                            <div className="font-normal tracking-tight">
+                                Caden Lund
+                            </div>
+                            <div className="text-lg font-normal text-zinc-400">
+                                Pro Gooner
+                            </div>
+                        </div>
                     </Link>
-                    <nav className="dark:bg-white dark:bg-opacity-5 py-3.5 px-8 rounded-full backdrop-blur-md border-white border border-opacity-10">
+                    <nav className="bg-white bg-opacity-5 py-3.5 px-8 rounded-full backdrop-blur-lg dark:border-white border-black border dark:border-opacity-10 border-opacity-20 shadow-md">
                         <ul className="flex gap-11">
                             <li>
                                 <NavLink
